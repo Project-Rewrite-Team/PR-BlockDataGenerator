e15ba0a04e4be5e6ecf2c14d3b9c394b38dae872
HavenSelph <havenselph@gmail.com>
2022-12-07 13:38:06 -0600

    Added more defaults, and classes to autogenerate them

--- PythonAutoGenerator.py
+++ PythonAutoGenerator.py
@@ -13,23 +13,47 @@ BRACKETS = "{}"
 
 
 # Initialize pycommands
-NOT_FOUND = f"{Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} was not found! Type {Colors.RED}help <command>{Colors.LIGHT_GRAY} for available commands/info on them!{Colors.END}"
-ARG_MISMATCH = f"{Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} takes {Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} argument(s), but {Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} were passed!{Colors.END}"
-COMMANDMODULE = Commands(NOT_FOUND, ARG_MISMATCH)
+_NOT_FOUND = f"{Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} was not found! Type {Colors.RED}help <command>{Colors.LIGHT_GRAY} for available commands/info on them!{Colors.END}"
+_ARG_MISMATCH = f"{Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} takes {Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} argument(s), but {Colors.RED}{BRACKETS}{Colors.LIGHT_GRAY} were passed!{Colors.END}"
+COMMANDMODULE = Commands(_NOT_FOUND, _ARG_MISMATCH)
 
 # Global variables
 NAMESPACE = None
 
-# Attempt to load saved namespace
+# Will come in a future update!
+USECUSTOMDIRECTORY = False
+CUSTOMDIRECTORY = None
+
+# Attempt to load saved vars
 try:
     with open('save.pag', 'rb') as file:
-        NAMESPACE = load(file)
+        NAMESPACE, USECUSTOMDIRECTORY, CUSTOMDIRECTORY = load(file)
 except TypeError as e:
     print("Error loading saved namespace! If this is your first time using the program, you can ignore this error.")
     print(e)
+except FileNotFoundError:
+    pass
+except Exception as e:
+    print(e)
     
 # Internal functions for use in commands
-# ...
+def genericRegisterWithItem(blockname):
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "blockstates", "default.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "blockstates"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+        }
+    )
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "models", "item", "default.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "model", "item"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:item/{blockname}",
+        }
+    )
 
 
 # Add all commands to be used
@@ -41,7 +65,7 @@ def help():
 @COMMANDMODULE.add_command("save")
 def save():
     with open('save.pag', 'wb') as file:
-        dump(NAMESPACE, file)
+        dump([NAMESPACE, USECUSTOMDIRECTORY, CUSTOMDIRECTORY], file)
     print(f"{Colors.GREEN}Save complete!{Colors.END}")
 
 @COMMANDMODULE.add_command("exit", aliases=["quit"])
@@ -65,14 +89,44 @@ def setnamespace(name=None):
 def namespace():
     return f"Current namespace is {Colors.GREEN}{NAMESPACE}{Colors.END}."
 
+@COMMANDMODULE.add_command("togglecustomdirectory")
+def togglecustomdirectory():
+    global USECUSTOMDIRECTORY
+    if USECUSTOMDIRECTORY:
+        USECUSTOMDIRECTORY = False
+    else:
+        if CUSTOMDIRECTORY:
+            USECUSTOMDIRECTORY = True
+        else:
+            return f"You must set a custom directory, use {Colors.RED}setcustomdirectory <path>{Colors.END}!"
+
 @COMMANDMODULE.add_command("clearscreen", aliases=["clear","cls","clr"])
 def clearscreen():
     return "\033[2J\033[H"
 
 @COMMANDMODULE.add_command("newpillarblock <blockname>", aliases=["pillarblock","addpillarblock","generatepillarblock"])
-def newpillarblock(blockname):
+def newpillarblock(blockname, dropped_block=None):
     if not NAMESPACE:
         return f"{Colors.LIGHT_GRAY}You need to set a namespace: {Colors.RED}setnamespace <namespace>{Colors.END}"
+    if not dropped_block:
+        FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "data", "loot_tables", "blocks", "drops_self.json"),
+        newfilepath=path.join(getcwd(), "Generated", "data", NAMESPACE, "loot_tables", "blocks"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+        }
+    )
+    else:
+        FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "data", "loot_tables", "blocks", "stone_like.json"),
+        newfilepath=path.join(getcwd(), "Generated", "data", NAMESPACE, "loot_tables", "blocks"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+            "<02>":dropped_block
+        }
+    )
     FileReplacer.FileReplacer.make_new_file(
         copyfilepath=path.join(getcwd(), "Default", "assets", "blockstates", "pillaredblock.json"),
         newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "blockstates"),
@@ -108,6 +162,77 @@ def newpillarblock(blockname):
         }
     )
 
+@COMMANDMODULE.add_command("sign <signname>")
+def sign(blockname):
+    if not NAMESPACE:
+        return f"{Colors.LIGHT_GRAY}You need to set a namespace: {Colors.RED}setnamespace <namespace>{Colors.END}"
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "blockstates", "sign.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "blockstates"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+        }
+    )
+    newblockname = blockname.split("_")
+    newblockname.insert(2, "wall")
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "blockstates", "wall_sign.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "blockstates"),
+        newfilename="_".join(newblockname)+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+        }
+    )
+    newblockname = blockname.split("_")
+    newblockname[2] = "planks"
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "models", "block", "sign.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "models", "block"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{'_'.join(newblockname)}",
+        }
+    )
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "models", "item", "default.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "models", "item"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:item/{blockname}",
+        }
+    )
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "data", "loot_tables", "blocks", "drops_self.json"),
+        newfilepath=path.join(getcwd(), "Generated", "data", NAMESPACE, "loot_tables", "blocks"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:item/{blockname}",
+        }
+    )
+    
+
+@COMMANDMODULE.add_command("leaves <sapling>")
+def leaves(blockname, sapling):
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "data", "loot_tables", "blocks", "leaves.json"),
+        newfilepath=path.join(getcwd(), "Generated", "data", NAMESPACE, "loot_tables", "blocks"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+            "<02>":sapling
+        }
+    )
+    FileReplacer.FileReplacer.make_new_file(
+        copyfilepath=path.join(getcwd(), "Default", "assets", "models", "block", "leaves.json"),
+        newfilepath=path.join(getcwd(), "Generated", "assets", NAMESPACE, "models", "block"),
+        newfilename=blockname+".json",
+        replacekeywords= {
+            "<01>":f"{NAMESPACE}:block/{blockname}",
+        }
+    )
+    genericRegisterWithItem(blockname)
+    
 
 try:
     print(f"{Colors.LIGHT_GRAY}Welcome to the autogenerator, type {Colors.GREEN}help <command>{Colors.LIGHT_GRAY} for available commands/info on them!"+Colors.END)
